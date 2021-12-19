e9d7c81508113c9fea8a2b450d827ecaf8a35873
Macgizmo <maciejgizowski@op.pl>
2021-12-19 09:49:27 +0100

    modół

--- Pakiet/main.py
+++ Pakiet/main.py
@@ -3,3 +3,16 @@ d1 = datetime.date(2019,8,15)
 print(d1)
 dh1 = datetime.datetime (2019,8,14,5,12,10)
 print(dh1)
+#%%
+import datetime
+x1 = datetime.date(2019,1,1)
+x2 = datetime.date(2021,8,9)
+x3=x2-x1
+print(x3)
+#%%
+import datetime
+x1 = datetime.date(1980,2,10)
+x2 = datetime.date(2021,12,19)
+x4 = x2-x1
+print(x4)
+# %%
